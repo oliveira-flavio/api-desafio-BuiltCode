defd079aeec3244be4a49344f5e73fcdfd606250
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-21 10:17:47 -0300

    Adiciona viewmodels (dto)

---
+++ src/Teste.Api/ViewModels/MedicoViewModel.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Api.ViewModels
+{
+    public class MedicoViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(255, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
+        public string Nome { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(50, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+        public string Crm { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(2, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string UfCrm { get; set; }
+        [Required(ErrorMessage = "O campo {0} é obrigatório")]
+        [StringLength(255, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 3)]
+        public string Especialidade { get; set; }
+        public List<PacienteViewModel> Pacientes { get; set; }
+    }
+}
---
+++ src/Teste.Api/ViewModels/PacienteViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Api.ViewModels
+{
+    public class PacienteViewModel
+    {
+        [Key]
+        public Guid Id { get; set; }
+        [Required]
+        public Guid MedicoId { get; set; }
+        [StringLength(255)]
+        public string Nome { get; set; }
+        [Required]
+        [StringLength(11, ErrorMessage = "O campo {0} precisa ter {1} caracteres", MinimumLength = 11)]
+        public string Cpf { get; set; }
+        [StringLength(20)]
+        public string Telefone { get; set; }
+        [ScaffoldColumn(false)]
+        public string NomeMedico { get; set; }
+    }
+}
