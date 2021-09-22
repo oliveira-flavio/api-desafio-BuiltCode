a6aca16d8fd0542c18d512e86415995e7287ddac
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 15:36:20 -0300

    Adiciona repository principal e medicorepository

---
+++ src/Teste.Data/Repository/MedicoRepository.cs
@@ -0,0 +1,17 @@
+﻿using Desafio.Business.Interfaces;
+using Desafio.Business.Models;
+using Desafio.Data.Context;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Data.Repository
+{
+    public class MedicoRepository : Repository<Medico>, IMedicoRepository
+    {
+        public MedicoRepository(DesafioDbContext context) : base(context)
+        {
+        }
+    }
+}
