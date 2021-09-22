a38d1574c942e40e5f54aa21dc22cfd12e8875b0
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 15:34:49 -0300

    Adiciona interface medicorepository e repository

---
+++ src/Teste.Business/Interfaces/IMedicoRepository.cs
@@ -0,0 +1,12 @@
+﻿using Desafio.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Business.Interfaces
+{
+    public interface IMedicoRepository : IRepository<Medico>
+    {
+    }
+}
