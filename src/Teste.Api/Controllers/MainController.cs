fb1d4f164e57eaf48261f231e85d22bfe75c13cd
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 15:33:54 -0300

    Adiciona controller main e medico

---
+++ src/Teste.Api/Controllers/MainController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace Desafio.Api.Controllers
+{
+
+    [ApiController]
+    public abstract class MainController : ControllerBase
+    {
+        //validação de notificações de erro
+        //validação de modelstate
+        //validação de operação de negócios
+    }
+}
---
+++ src/Teste.Api/Controllers/MedicosController.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using Desafio.Api.ViewModels;
+using Desafio.Business.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Desafio.Api.Controllers
+{
+    [Route("api/[controller]")]
+    public class MedicosController : MainController
+    {
+        private readonly IMedicoRepository _medicoRepository;
+        private readonly IMapper _mapper;
+
+        public MedicosController(IMedicoRepository medicoRepository, IMapper mapper)
+        {
+            _medicoRepository = medicoRepository;
+            _mapper = mapper;
+        }
+        public async Task<ActionResult<IEnumerable<MedicoViewModel>>> ObterTodos()
+        {
+            var medico = _mapper.Map<IEnumerable<MedicoViewModel>>(await _medicoRepository.ObterTodos());
+            return Ok(medico);
+        }
+
+    }
+}
