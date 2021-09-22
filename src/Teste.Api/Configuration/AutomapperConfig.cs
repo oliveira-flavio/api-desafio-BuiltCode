451dd946a114e050c4e5b9892dff151905b09ef2
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 15:33:30 -0300

    Adiciona automapper e configuração de dependency injection

---
+++ src/Teste.Api/Configuration/AutomapperConfig.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using Desafio.Api.ViewModels;
+using Desafio.Business.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Api.Configuration
+{
+    public class AutomapperConfig : Profile
+    {
+        public AutomapperConfig()
+        {
+            CreateMap<Medico, MedicoViewModel>().ReverseMap();
+            CreateMap<Paciente, PacienteViewModel>().ReverseMap();
+            CreateMap<Usuario, UsuarioViewModel>().ReverseMap();
+        }    
+    }
+}
---
+++ src/Teste.Api/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,22 @@
+﻿using Desafio.Business.Interfaces;
+using Desafio.Data.Context;
+using Desafio.Data.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Api.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<DesafioDbContext>();
+            services.AddScoped<IMedicoRepository, MedicoRepository>();
+
+            return services;
+        }
+    }
+}
