0bf788db3c039e0a357cece36d07ba2f37ab9c43
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-22 15:35:50 -0300

    Adiciona inicio de configuração da classe DesafioDbContext

---
+++ src/Teste.Data/Context/DesafioDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Desafio.Business.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Data.Context
+{
+    public class DesafioDbContext : DbContext
+    {
+        public DesafioDbContext(DbContextOptions<DesafioDbContext> options) : base(options) { }
+
+        public DbSet<Medico> Medicos { get; set; }
+        public DbSet<Paciente> Pacientes { get; set; }
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Parceiro> Parceiros { get; set; }
+
+
+    }
+}
