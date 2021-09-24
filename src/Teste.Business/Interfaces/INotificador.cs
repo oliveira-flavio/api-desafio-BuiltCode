30097d70a397b6622ff73fccfc5a3960200df9a0
Flávio Oliveira <flavio_r_o@yahoo.com.br>
2021-09-24 11:07:59 -0300

    Adiciona interface de notificação

---
+++ src/Teste.Business/Interfaces/INotificador.cs
@@ -0,0 +1,15 @@
+﻿using Desafio.Business.Notificacoes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Desafio.Business.Interfaces
+{
+    public interface INotificador
+    {
+        bool TemNotificacao();
+        List<Notificacao> ObterNotificacoes();
+        void Handle(Notificacao notificacao);
+    }
+}
